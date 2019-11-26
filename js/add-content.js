0885a22c10643e2ee0a2f53c13a50dc90fc9e6c9
faithgarza <faith.garza001@gmail.com>
2019-11-25 21:13:17 -0600

    worked on some bugs

---
+++ js/add-content.js
@@ -0,0 +1,17 @@
+/* Thi s script displays a greeting to the user based upon the current time. It is an example from JavaScript &jQuery book */
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning';
+} else {
+    greeting = 'Welcome';
+}
+
+document.write('<h3>' + greeting + '</h3>');
